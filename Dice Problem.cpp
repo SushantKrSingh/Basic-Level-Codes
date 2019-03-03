aab8c950c3527d9c4a8dd41eba886586447dc7d1
Sushant Kumar <ssushant328@gmail.com>
2019-03-03 11:07:07 +0530

    The Dice Problem
    C++ program to solve Dice Problem

---
+++ Dice Problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int R;
+    cin>>R;
+    while(R--)
+    {
+        int a;
+        cin>>a;
+        cout<<7-a<<endl;
+    }
+	return 0;
+}
