be92c25fc56b44de28941ee8e986d6dc67cb73b9
Sushant Kumar <ssushant328@gmail.com>
2019-03-03 10:58:57 +0530

    Add files via upload
    C++ Code for Printing String N times

---
+++ Printing Strings 'n' time.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+	    int i,N;
+	    cin>>N;
+	    for(i=0;i<N;i++)
+	    {
+	        cout<<"Your Output Here"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
