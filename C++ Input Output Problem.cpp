59001da101db64684fbd0c8a008476722e89f50d
Sushant Kumar <ssushant328@gmail.com>
2019-03-03 11:10:19 +0530

    C++ Problem on Input/Output
    C++ Code to solve problem

---
+++ C++ Input Output Problem.cpp
@@ -0,0 +1,14 @@
+//Date:-22 August 2018
+#include<iostream>
+using namespace std;
+int main()
+{
+    int test,a,b;
+    cin>>test;
+    for(int i=0;i<test;i++)
+    {
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+      return 0;
+}
