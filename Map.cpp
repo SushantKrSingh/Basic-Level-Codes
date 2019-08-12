02810bcfb891b75b29317da57e0172b78a125d4b
Sushant Kumar <ssushant328@gmail.com>
2019-08-12 09:31:16 +0530

    World Map using C++

---
+++ Map.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	
+	for(b=c=10; a="Hello! Welcome .\
+	TFy ! QJu ROo TNn(ROo)SLq ULo+\
+	 UHs UJq TNn*RPn/QPbEWS-JSWQAIJO^\
+	 NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
+	 HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
+	 T|S~Pn SPm SOn TNn ULoOULo#ULo-W\
+	 Hq!WFs XDt!" [b+++21];)
+	 
+	 for(;a-- >64; )
+	 putchar (++c=='Z'?c =c/ 9:33^b&1);
+	 
+	 return 0;
+}
