0d039273c8d7de7bfa9a30adbdba7b134f1cd72f
Sushant Kumar <ssushant328@gmail.com>
2019-08-21 19:59:17 +0530

    Even No. from 0 to 100 using do-while loop

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Even
+{
+public static void main(String arg[])
+{
+int i=0;
+Scanner obj=new Scanner(System.in);
+System.out.println("Even Number.");
+do
+{
+System.out.println("Number is:"+i);
+i=i+2;
+}
+while(i<=100);
+}
+}
